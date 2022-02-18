1507f83891e2710df2031cef86f186b4aff3ebe6
Amit Pratap Singh <samit5585@gmail.com>
2022-02-18 23:22:34 +0530

    testing

--- src/app/router/index.js
+++ src/app/router/index.js
@@ -1,31 +1,30 @@
 // import Vue from 'vue'; 
-
 // import Router from 'vue-router';
 // import * as Vue from 'vue';
 import * as VueRouter from 'vue-router';
 import UserLogin from '../auth/views/UserLogin.vue'
 import Login from '../auth/views/Login.vue'
 import ChangePassword from '../auth/views/ChangePassword.vue'
-// Vue.use(VueRouter);
+Vue.use(VueRouter);
 
- 
-const routes=[
+
+const routes = [
     {
-        path:'/',
-        component:UserLogin
+        path: '/',
+        component: UserLogin
     },
     {
-        path:'/login',
-        component:Login
+        path: '/login',
+        component: Login
     },
     {
-        path:'/changepassword',
-        component:ChangePassword
+        path: '/changepassword',
+        component: ChangePassword
     }
 ]
- 
-const router =  VueRouter.createRouter({
+
+const router = VueRouter.createRouter({
     routes // short for `routes: routes`
 })
 
- export default router
+export default router
